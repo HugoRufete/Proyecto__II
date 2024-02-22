502f19b19d551009f0d6484cac6711a2aa503397
MRamon04 <marcosramon2004@gmail.com>
2024-02-22 14:25:11 +0100

    Logica mirar hacia cursor
    En el inspector de jugador tendreis que meter la MainCamera

--- Load-In/Assets/Scripts/SimplePlayerMovement.cs
+++ Load-In/Assets/Scripts/SimplePlayerMovement.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class SimplePlayerMovement : MonoBehaviour
 {
-
+    //Variables de movimiento
     public float movSpeed;
     float SpeedX, SpeedY;
+
+    //Hacia donde va a mirar el personaje
+    private Vector3 objective;
+    [SerializeField] private new Camera camera;
+
+
     Rigidbody2D rb;
 
     void Start()
@@ -16,9 +22,27 @@ public class SimplePlayerMovement : MonoBehaviour
 
     void Update()
     {
-        SpeedX = Input.GetAxisRaw("Horizontal") * movSpeed;        
-        SpeedY = Input.GetAxisRaw("Vertical") * movSpeed;        
+        Movement();
+    }
 
-        rb.velocity = new Vector2 (SpeedX, SpeedY);
+    //Movimiento simple de personaje + rotacion de este a traves del cursor
+    void Movement()
+    {
+        //Cogemos las inputs y las multiplicamos por la velocidad
+        SpeedX = Input.GetAxisRaw("Horizontal") * movSpeed;
+        SpeedY = Input.GetAxisRaw("Vertical") * movSpeed;
+
+        //Le aplicamos la velocidad al RB
+        rb.velocity = new Vector2(SpeedX, SpeedY);
+
+        //Indicamos donde se encuentra el objetivo al que queremos mirar
+        objective = camera.ScreenToWorldPoint(Input.mousePosition);
+        float radAngle = Mathf.Atan2(objective.y - transform.position.y, objective.x - transform.position.x);
+        float degAngle = (180/ Mathf.PI)*radAngle - 90;
+
+        //Cambiamos la rotacion del personaje dependiendo de donde esta el objetivo
+        transform.rotation = Quaternion.Euler(0,0,degAngle);
+
+
     }
 }
