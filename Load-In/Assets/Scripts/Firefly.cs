5c9dfc2b852175834cd31a0aec7be1835e4cf5b4
MRamon04 <marcosramon2004@gmail.com>
2024-02-25 20:27:18 +0100

    Enemigo luciernaga 1a parte

--- Load-In/Assets/Scripts/Firefly.cs
+++ Load-In/Assets/Scripts/Firefly.cs
@@ -2,15 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Firefly : MonoBehaviour
 {
     public float followSpeed = 5f; // Velocidad de seguimiento
     private Transform player; // Referencia al transform del jugador
+
+    private Rigidbody2D rb2d;
+
+
+    [SerializeField] private float radiousToExplode; //Radio del area de detecciion para la explosión
+    
+
+
 
     void Start()
     {
         // Encuentra al jugador al comienzo
         player = GameObject.FindWithTag("Player").transform;
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -24,5 +33,15 @@ public class Enemy : MonoBehaviour
             // Mueve al enemigo en la dirección del jugador con la velocidad específica
             transform.Translate(direction * followSpeed * Time.deltaTime);
         }
+
+
+        //Si el radio de explosion es menor a la distancia con el jugador
+        if (radiousToExplode <= transform.position.x - player.transform.position.x  && radiousToExplode <= transform.position.y - player.transform.position.y)
+        {
+            followSpeed = 0f;
+            //animación de explotar
+            
+        }
+        
     }
 }
