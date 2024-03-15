c7ceb688db8b5ffa7349b0839b6ac3f216b58ab0
HugoRufete <hugorufete@gmail.com>
2024-03-15 10:19:15 +0100

    Player Movement Actualizado

--- Load-In/Assets/Scripts/Player/SimplePlayerMovement.cs
+++ Load-In/Assets/Scripts/Player/SimplePlayerMovement.cs
@@ -12,17 +12,20 @@ public class SimplePlayerMovement : MonoBehaviour
     private Vector3 objective;
     [SerializeField] private new Camera camera;
 
+    private SpriteRenderer spriteRenderer; // Referencia al SpriteRenderer
 
     Rigidbody2D rb;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();        
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>(); // Obtener la referencia al SpriteRenderer
     }
 
     void Update()
     {
         Movement();
+        UpdateLayer(); // Llamar a la función para actualizar la capa del sprite
     }
 
     //Movimiento simple de personaje + rotacion de este a traves del cursor
@@ -35,10 +38,6 @@ public class SimplePlayerMovement : MonoBehaviour
         //Le aplicamos la velocidad al RB
         rb.velocity = new Vector2(SpeedX, SpeedY);
 
-        /*/
-        float radAngle = Mathf.Atan2(objective.y - transform.position.y, objective.x - transform.position.x);
-        float degAngle = (180 / Mathf.PI) * radAngle - 90;
-*/
         //Indicamos donde se encuentra el objetivo al que queremos mirar
         objective = camera.ScreenToWorldPoint(Input.mousePosition);
         //Cambiamos la rotacion del personaje dependiendo de donde esta el objetivo
@@ -50,8 +49,20 @@ public class SimplePlayerMovement : MonoBehaviour
         {
             transform.eulerAngles = new Vector3(0, 0, 0);
         }
-        //transform.rotation = Quaternion.Euler(0, 0, degAngle);
+    }
 
+    // Función para actualizar la capa del sprite
+    void UpdateLayer()
+    {
+        Vector3 directionToMouse = (objective - transform.position).normalized;
 
+        if (directionToMouse.y > 0) // Si el ratón está por encima del jugador
+        {
+            spriteRenderer.sortingOrder = 3; // Cambiar la capa a 3 (delante)
+        }
+        else // Si el ratón está por debajo del jugador
+        {
+            spriteRenderer.sortingOrder = 1; // Cambiar la capa a 1 (detrás)
+        }
     }
 }
