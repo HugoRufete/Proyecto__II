c93f25fa27ab8b11618db830f18189969babbd02
HugoRufete <hugorufete@gmail.com>
2024-03-20 17:16:10 +0100

    Revert "Import Arte Weapon_Wheel + Ajustes Armas"
    This reverts commit c51ee76352eb062c85c68359826939fb4a0fa22c.

--- Load-In/Assets/Equipar.cs
+++ Load-In/Assets/Equipar.cs
@@ -2,34 +2,47 @@ using UnityEngine;
 
 public class Equipar : MonoBehaviour
 {
-    public GameObject basic_weapon;  
-    public GameObject sword; 
-    public GameObject shotgun;  
-    public Transform puntoDeEquipamiento;
-    public GameObject desactivarHUD;
+    public GameObject basic_weapon;  // Asigna el prefab del arma desde el Inspector
+    public GameObject sword;  // Asigna el prefab del arma desde el Inspector
+    public GameObject shotgun;  // Asigna el prefab del arma desde el Inspector
+    public Transform puntoDeEquipamiento;  // Punto donde se instanciarán las armas
 
-   
+    void Update()
+    {
+        // Ejemplo: Presionar la tecla "E" para equipar el arma
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            EquiparShotGun();
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            EquiparSword();
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            EquiparBasicWeapon();
+        }
 
-    public void EquiparShotGun()
+    }
+
+    void EquiparShotGun()
     {
         if (shotgun != null)
         {
-            
+            // Encuentra el objeto del jugador (asume que solo hay un objeto de jugador en la escena)
             GameObject jugador = GameObject.FindGameObjectWithTag("Player");
 
             if (jugador != null)
             {
-                
+                // Instancia el prefab del arma y lo hace hijo del jugador
                 Instantiate(shotgun, puntoDeEquipamiento.position, Quaternion.identity, jugador.transform);
-                desactivarHUD.SetActive(false);
-                Time.timeScale = 1.0f;
                 Debug.Log("Arma equipada");
                 
             }
         }
     }
 
-    public void EquiparSword()
+    void EquiparSword()
     {
         if (sword != null)
         {
@@ -38,7 +51,7 @@ public class Equipar : MonoBehaviour
 
             if (jugador != null)
             {
-            
+            // Instancia el prefab del arma con una rotación de -45 grados en Z y lo hace hijo del jugador
             Instantiate(sword, puntoDeEquipamiento.position, Quaternion.Euler(0f, 0f, -45f), jugador.transform);
             Debug.Log("Arma equipada");
             }
@@ -46,16 +59,16 @@ public class Equipar : MonoBehaviour
     
     }
 
-    public void EquiparBasicWeapon()
+    void EquiparBasicWeapon()
     {
         if (basic_weapon != null)
         {
-           
+            // Encuentra el objeto del jugador (asume que solo hay un objeto de jugador en la escena)
             GameObject jugador = GameObject.FindGameObjectWithTag("Player");
 
             if (jugador != null)
             {
-                
+                // Instancia el prefab del arma con una rotación de -45 grados en Z y lo hace hijo del jugador
                 Instantiate(basic_weapon, puntoDeEquipamiento.position, Quaternion.Euler(0f, 0f, -45f), jugador.transform);
                 Debug.Log("Arma equipada");
             }
