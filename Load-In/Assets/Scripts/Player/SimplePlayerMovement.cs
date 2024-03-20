c5687f385bd003f79ae95ca3ed3c74e752983098
MRamon04 <marcosramon2004@gmail.com>
2024-03-20 14:20:57 +0100

    Segundo enemigo logica movimiento + mejoras de anim

--- Load-In/Assets/Scripts/Player/SimplePlayerMovement.cs
+++ Load-In/Assets/Scripts/Player/SimplePlayerMovement.cs
@@ -38,14 +38,7 @@ public class SimplePlayerMovement : MonoBehaviour
 
         //Le aplicamos la velocidad al RB
         rb.velocity = new Vector2(SpeedX, SpeedY);
-        if (SpeedX != 0 || SpeedY != 0)
-        {
-            anim.Play("Walk");
-        }
-        else
-        {
-            anim.Play("Idle");
-        }
+        
 
         //Indicamos donde se encuentra el objetivo al que queremos mirar
         objective = camera.ScreenToWorldPoint(Input.mousePosition);
@@ -57,6 +50,34 @@ public class SimplePlayerMovement : MonoBehaviour
         if (objective.x > transform.position.x)
         {
             transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        
+        if (SpeedX != 0 || SpeedY != 0)
+        {
+            if (SpeedX > 0 && objective.x < transform.position.x)
+            {
+                anim.Play("Back_Walk");
+            }
+            if (SpeedX > 0 && objective.x > transform.position.x)
+            {
+                anim.Play("Walk");
+            }
+            if (SpeedX < 0 && objective.x > transform.position.x)
+            {
+                anim.Play("Back_Walk");
+            }
+            if (SpeedX < 0 && objective.x < transform.position.x)
+            {
+                anim.Play("Walk");
+            }
+            if (SpeedY != 0 && SpeedX == 0)
+            {
+                anim.Play("Walk");
+            }
+        }
+        else
+        {
+            anim.Play("Idle");
         }
     }
 
