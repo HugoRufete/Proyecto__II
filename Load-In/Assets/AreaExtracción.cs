dd41f9cc7c9b30d5ae5ed7983b79faa2f8f7abed
HugoRufete <hugorufete@gmail.com>
2024-03-20 17:13:26 +0100

    Import Arte Weapon_Wheel + Ajustes Armas

---
+++ Load-In/Assets/AreaExtracción.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaExtracción : MonoBehaviour
+{
+    private bool playerInside = false;
+    public float extractionTimer = 0f;
+    private float extractionDuration = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInside)
+        {
+            extractionTimer += Time.deltaTime;
+
+            if (extractionTimer >= extractionDuration)
+            {
+                Debug.Log("¡Extracción completada!");
+                extractionTimer = 0f; // Reiniciar el temporizador
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInside = true;
+            Debug.Log("El jugador ha entrado en el area de extracción");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInside = false;
+            extractionTimer = 0f; 
+        }
+    }
+}
