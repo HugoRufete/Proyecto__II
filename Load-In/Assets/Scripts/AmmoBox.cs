0b5401caa9ffcb6865f0df21b24374865ae0da75
HugoRufete <hugorufete@gmail.com>
2024-05-05 23:12:49 +0200

    Area Extracción + Ajustes

--- Load-In/Assets/Scripts/AmmoBox.cs
+++ Load-In/Assets/Scripts/AmmoBox.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
 
 public class AmmoBox : MonoBehaviour
 {
     private List<IRecargable> _recargables;
 
+    private Animator anim;
+
+    private void Start()
+    {
+        GameObject targetObject = GameObject.FindWithTag("PopUps");
+        anim = targetObject.GetComponent<Animator>();
+
+       
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            anim.Play("AmmunitionReloadedAnimation");
             FindAndAssignRecargables(collision.gameObject.transform);
-
             foreach (var recargable in _recargables)
             {
                 recargable.RecargarArma();
@@ -39,6 +50,16 @@ public class AmmoBox : MonoBehaviour
         if (_recargables.Count == 0 && root == null)
         {
             Debug.LogError("No se encontraron armas recargables en la escena.");
+        }
+    }
+
+    private IEnumerator DestroyObjectCoroutine(GameObject objetoADestruir, float delayInSeconds)
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+
+        if (objetoADestruir != null)
+        {
+            objetoADestruir.SetActive(false);
         }
     }
 }
--- Load-In/Assets/Scripts/AreaExtracción.cs
+++ Load-In/Assets/Scripts/AreaExtracción.cs
@@ -2,16 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class AreaExtracción : MonoBehaviour
 {
     private bool playerInside = false;
     public float extractionTimer = 0f;
-    private float extractionDuration = 5f;
+    private float extractionDuration = 15f;
+
+    public GameObject timerExtraction;
+
+    public Animator animator;
 
     // Update is called once per frame
+
+    public TMP_Text extractionTimerText;
     void Update()
     {
+        
+           
+        
         if (playerInside)
         {
             extractionTimer += Time.deltaTime;
@@ -22,6 +32,7 @@ public class AreaExtracción : MonoBehaviour
                 Debug.Log("¡Extracción completada!");
                 extractionTimer = 0f; // Reiniciar el temporizador
             }
+            extractionTimerText.text = "You will scape in 15 seconds! " + extractionTimer.ToString("F2") + " seconds";
         }
     }
 
@@ -29,6 +40,9 @@ public class AreaExtracción : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
+            timerExtraction.SetActive(true);
+            animator.enabled = true;
+            animator.Play("AreaExtracciónAnimation");
             playerInside = true;
             Debug.Log("El jugador ha entrado en el area de extracción");
         }
@@ -38,6 +52,8 @@ public class AreaExtracción : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
+            timerExtraction.SetActive(false);
+            animator.Play("AreaExtracciónIdleAnimation");
             playerInside = false;
             extractionTimer = 0f; 
         }
